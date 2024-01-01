4dcd220d56046d017a00e26afd62425dc1db8271
Setha Va <vasetha1@gmail.com>
2024-01-01 23:14:38 +0700

    (style): home page

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const onClick = () => {
+    console.log("Login Button Clicked");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO: Implement modal</span>;
+  }
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
