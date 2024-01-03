ed1ce3ecd9af14a6a9db221a22dcea1505be71d8
Setha Va <vasetha1@gmail.com>
2024-01-04 00:10:01 +0700

    set up next auth callback

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import authConfig from "@/auth.config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "@/lib/db";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +12,37 @@ export const {
   signOut,
   signIn,
 } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,20 @@
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    role?: UserRole;
+  }
+}
