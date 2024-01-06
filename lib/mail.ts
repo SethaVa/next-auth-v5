d7ac41590aaf13cc0d7f673f08226cc0813008e7
Setha Va <vasetha1@gmail.com>
2024-01-06 22:57:20 +0700

    Email verification

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,8 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
-
-  console.log("send");
+ 
   await resend.emails.send({
     from: "vasetha1@gmail.com",
     to: email,
