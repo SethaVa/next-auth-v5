81a91cf56874bdf848b4b14beab1d7fc9eeafb06
Setha Va <vasetha1@gmail.com>
2024-01-10 12:51:40 +0700

    (style) change background color

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-tr from-purple-500 via-indigo-500 to-sky-500">
       <div className="space-y-6 text-center">
         <h1
           className={cn(
